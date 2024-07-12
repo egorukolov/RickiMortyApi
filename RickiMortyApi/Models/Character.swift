f41a4f78fbb6826cf2aeefef4327729bb61e805b
Egor Ukolov <ukolovegor@yandex.ru>
2024-07-12 14:49:37 +0700

    Настройка проекта, import фреймфорка Alamofare в проект

---
+++ RickiMortyApi/Models/Character.swift
@@ -0,0 +1,8 @@
+//
+//  Character.swift
+//  RickiMortyApi
+//
+//  Created by Egor Ukolov on 12.07.2024.
+//
+
+import Foundation
---
+++ RickiMortyApi/NetworkManagers/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  RickiMortyApi
+//
+//  Created by Egor Ukolov on 12.07.2024.
+//
+
+import Foundation
