fbd6e980c366565ae6a2b9d4296b7d8ef3042d6d
Egor Ukolov <ukolovegor@yandex.ru>
2024-07-12 13:50:53 +0700

    Initial Commit

---
+++ RickiMortyApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickiMortyApi
+//
+//  Created by Egor Ukolov on 12.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
